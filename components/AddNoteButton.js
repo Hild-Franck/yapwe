96d1de7a441e5c328977fe15a30b4ef4b2469e77
Knarfux <franck.hild@gmail.com>
2020-08-09 18:54:49 -0400

    Update add note button icon

--- components/AddNoteButton.js
+++ components/AddNoteButton.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
-import PersonAddIcon from '@material-ui/icons/PersonAdd'
+import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd'
 
 import { setData } from '../ducks/modal'
 
 const AddNoteButton = ({ dispatch, year, month }) => <Button
   variant="contained"
   color="primary"
-  startIcon={<PersonAddIcon>add</PersonAddIcon>}
+  startIcon={<PlaylistAddIcon>add</PlaylistAddIcon>}
   onClick={() => dispatch(setData('note', { date: new Date(year, month, new Date().getUTCDate()) }))}
 >
   Add
